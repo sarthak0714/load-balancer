ab55a8bc59566dbd290c91b558c1b94af6ddf07c
= <=>
2024-02-17 20:34:06 +0530

    added server next logic

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/sarthak0714/load-balancer
 
-go 1.21.5
+go 1.22.0
--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 	"sync/atomic"
 )
 
+const (
+	Attempts = iota
+	Retry
+
+)
+
 type Server struct {
 	URL          *url.URL
 	Alive        bool
@@ -18,26 +24,6 @@ type ServerPool struct {
 	servers []*Server
 	current uint64
 }
-
-func (s *ServerPool) Next() int {
-	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.servers)))
-}
-
-func (s *ServerPool) GetNext() *Server {
-	next := s.Next()
-	l := len(s.servers) + 1
-	for i := next; i < l; i++ {
-		idx := i % len(s.servers)
-		if s.servers[idx].IsAlive() {
-			if i != next {
-				atomic.StoreUint64(&s.current, uint64(idx))
-			}
-			return s.servers[idx]
-		}
-	}
-	return nil
-}
-
 func (s *Server) SetAlive(alive bool) {
 	s.mux.RLock()
 	s.Alive = alive
@@ -50,3 +36,32 @@ func (s *Server) IsAlive() (alive bool) {
 	s.mux.RUnlock()
 	return
 }
+
+func (s *ServerPool) MarkServerStatus(serverUrl *url.URL,alive bool){
+	for _,svr:= range s.servers{
+		if svr.URL.String()==serverUrl.String(){
+			svr.SetAlive(alive)
+			break
+		}
+	} 
+}
+
+func (s *ServerPool) NextIdx() int {
+	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.servers)))
+}
+
+func (s *ServerPool) GetNext() *Server {
+	next:=s.NextIdx()
+	l:=len(s.servers)+next
+	for i:= range l{
+		idx:= i%len(s.servers)
+		if s.servers[idx].IsAlive(){
+			if i!=next{
+				atomic.AddUint64(&s.current,uint64(idx))
+			}
+			return s.servers[idx]
+		}
+	}
+	return nil
+}
+
